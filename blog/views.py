a6fd2630db1f9634cb1df76b4ff3f70325ad7df3
Alexandre <logan@localhost.localdomain>
2018-03-16 20:07:32 -0300

    terminado

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.http import HttpResponseRedirect, Http404, HttpResponseNotFound
+from django.contrib.auth.models import User
 from .models import Post
+from .forms import PostForm
 
 # import pdb
 
@@ -17,3 +20,20 @@ def post_detail(request, pk):
     post.save()
 
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def new_post(request):
+    if not request.user.is_authenticated():
+       # raise Http404
+       return HttpResponseNotFound('<h1>404 - Not found :(</h1>')
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
